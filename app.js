25a683e8257411cbae9fa9e96184a955ca7c8609
Staasie <ana.stay.stay@gmail.com>
2024-10-29 15:41:13 -0700

    self update

---
+++ app.js
@@ -0,0 +1,66 @@
+'use strict';
+let answer1 = prompt('staas has one pet? (yes or no)');
+    if (answer1 === 'no') {
+      document.write('<p>not quite! I have 2 pets, Loki and Zii.</p>');
+    } else {
+      document.write('<p>Not quite. I actually have 2 pets, Loki and Zii.</p>');
+    }
+
+    let answer2 = prompt('Staas work\'s 2 jobs? (yes or no)');
+    if (answer2 === 'yes') {
+      document.write('<p>Yep, I do, and yes, I am tired!!</p>');
+    } else {
+      document.write('<p>Actually, I work 2 jobs and could use a break!</p>');
+    }
+
+    let answer3 = prompt('Does Staas know sign language? (yes or no)');
+    if (answer3.toLowerCase() === 'yes') {
+      document.write('<p>Oh, you\'re good at this! That is correct!</p>');
+    } else {
+      document.write('<p>Nope, I studied it for over 5 years!</p>');
+    }
+
+    let answer4 = prompt('Is Staas\'s favorite color green? (yes or no)');
+    if (answer4.toLowerCase() === 'yes') {
+      document.write('<p>Heck yeah it is, how\'d ya know?</p>');
+    } else {
+      document.write('<p>Good try! Not many know this one.</p>');
+    }
+
+
+
+
+
+
+// let answer1 = prompt('Staas only has one pet?');
+// if (answer1 === false) {
+//   document.write('I have 2 pets, Loki and Zii');
+// } else {
+//   document.write('Not quite.');
+// }
+
+// let answer2 = confirm('Does Staas work 2 jobs?');
+// if (answer2 === true) {
+//   console.log('Yep I do and yes I am tired!!');
+// } else {
+//   console.log('I sure wish I didn\'t');
+// }
+
+// let answer3 = confirm('Does Staas know sign language?');
+// if (answer3 === true) {
+//   console.log('Oh your good at this! That is correct!');
+// } else {
+//   console.log('Nope I studied it for over 5 years!');
+// }
+
+// let answer4 = confirm('Is staas\s favorite color green?');
+// if (answer4 === true) {
+//   console.log('Heck yeah it is, how\'d ya know?');
+// } else {
+//   console.log('It was a good try, not many know this one!');
+// }
+
+// let first = false
+// let second = true
+// let third = true
+// let fourth = true
