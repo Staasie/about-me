eae45f9e016fae424ed4eb218cae3ae197659dc8
Staasie <ana.stay.stay@gmail.com>
2024-11-05 08:49:06 -0800

    updates for about me

--- app.js
+++ app.js
@@ -1,30 +1,46 @@
+function askQuestions() {
+  const userName = prompt("Hey, Whats your name?");
+  document.getElementById(`response`).innerHTML =
+  `Welcome, ${userName}! I am glad you came by!`;
+}
+
+
+
 'use strict';
+
 let answer1 = prompt('staas has one pet? (yes or no)');
-    if (answer1 === 'no') {
+    if (answer1.toLowerCase()=== 'No') {
       document.write('<p>not quite! I have 2 pets, Loki and Zii.</p>');
     } else {
       document.write('<p>Not quite. I actually have 2 pets, Loki and Zii.</p>');
     }
 
     let answer2 = prompt('Staas work\'s 2 jobs? (yes or no)');
-    if (answer2 === 'yes') {
+    if (answer2.toLowerCase() === 'Yes') {
       document.write('<p>Yep, I do, and yes, I am tired!!</p>');
     } else {
       document.write('<p>Actually, I work 2 jobs and could use a break!</p>');
     }
 
     let answer3 = prompt('Does Staas know sign language? (yes or no)');
-    if (answer3.toLowerCase() === 'yes') {
+    if (answer3.toLowerCase() === 'Yes') {
       document.write('<p>Oh, you\'re good at this! That is correct!</p>');
     } else {
       document.write('<p>Nope, I studied it for over 5 years!</p>');
     }
 
     let answer4 = prompt('Is Staas\'s favorite color green? (yes or no)');
-    if (answer4.toLowerCase() === 'yes') {
+    if (answer4.toLowerCase() === 'Yes') {
       document.write('<p>Heck yeah it is, how\'d ya know?</p>');
     } else {
       document.write('<p>Good try! Not many know this one.</p>');
+    }
+
+    let answer5 = prompt('does Staas play ukulele? (yes or no)');
+    if (answer5.toLowerCase() === 'Yes') {
+      document.write('<p> yep i do! </p>');
+    } else {
+      document.write('<p>Oh come on, you have no faith :) .</p>');
     }
 
 
